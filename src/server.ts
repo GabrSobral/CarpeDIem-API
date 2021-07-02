78e7f826c33c6a9f86c709f2d72debf00ee278ed
Sobraloser <Asgoth55@gmail.com>
2021-07-02 16:59:36 -0300

    feat: add database config with TypeORM

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import 'reflect-metadata'
+import './database/index'
 
 const PORT = process.env.PORT || 3333;
 const app = express();
+
+app.use(express.json())
 
 app.listen(PORT, () => console.log(`Server Started at port ${PORT}`));
