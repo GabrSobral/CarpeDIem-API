251277081012498807e0b5f1d35dc322e1e87788
Sobraloser <Asgoth55@gmail.com>
2021-07-03 00:05:24 -0300

    feat: add ErrorCatchet middleware

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 require('dotenv').config()
 import express from 'express';
+import 'express-async-errors'
+import { routes } from './routes';
 import 'reflect-metadata'
 import './database/index'
-import { routes } from './routes';
+
+import { ErrorCatcher } from './middlewares/ErrorCatcher';
 
 const PORT = process.env.PORT || 3333;
 const app = express();
 
 app.use(express.json())
 app.use(routes)
+app.use(ErrorCatcher)
 
 app.listen(PORT, () => console.log(`Server Started at port ${PORT}`));
