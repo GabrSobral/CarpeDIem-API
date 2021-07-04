d9fdb4508f21070ef49508f76758439b6c00b5e4
Sobraloser <Asgoth55@gmail.com>
2021-07-04 16:52:05 -0300

    feat: add relations on activity entity

--- src/entities/Activity.ts
+++ src/entities/Activity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
+import { Category } from "./Category";
 
 @Entity()
 export class Activity {
@@ -14,6 +15,10 @@ export class Activity {
 
   @Column()
   category: string
+
+  @JoinColumn({ name: 'category' })
+  @ManyToOne(() => Category)
+  JoinCategory: Category;
 
   constructor(){
     if(!this.id){
