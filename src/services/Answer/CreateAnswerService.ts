3e5b8f8fc1a7227505fca6789000156b3280c340
Sobraloser <Asgoth55@gmail.com>
2021-07-05 19:15:31 -0300

    refact: change select query statement

--- src/services/Answer/CreateAnswerService.ts
+++ src/services/Answer/CreateAnswerService.ts
@@ -1,19 +1,37 @@
 import { getCustomRepository } from "typeorm";
 import { AnswerRepository } from "../../repositories/AnswerRepository";
+import { QuestionRepository } from "../../repositories/QuestionRepository";
 
 interface CreateAnswerServiceProps {
   user: string;
-  category: string;
   question: string;
   answer: string;
 }
 
 class CreateAnswerService {
-  async execute({ user, category, question, answer }: CreateAnswerServiceProps){
+  async execute({ user, question, answer }: CreateAnswerServiceProps){
     const answerRepository = getCustomRepository(AnswerRepository)
+    const questionRepository = getCustomRepository(QuestionRepository)
 
-    const isAlreadyAnswered = await answerRepository.findOne({ where: [user, question] })
-    console.log(isAlreadyAnswered)
+    const isQuestionExists = await questionRepository.findOne(question)
+
+    if(!isQuestionExists){
+      throw new Error("This question not exist status:400")
+    }
+    const category = isQuestionExists.category
+
+    const isAlreadyAnswered = await answerRepository
+    .createQueryBuilder('answer')
+    .where('answer.user = :user', { user })
+    .andWhere('answer.question = :question', { question })
+    .getOne()
+
+    if(isAlreadyAnswered){
+      isAlreadyAnswered.answer = answer
+      await answerRepository.save(isAlreadyAnswered)
+
+      return isAlreadyAnswered
+    }
 
     const newAnswer = answerRepository.create({ user, category, question, answer })
     await answerRepository.save(newAnswer)
