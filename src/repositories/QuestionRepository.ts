ae1fba94f509c152556b434261b3ef073f428c80
Sobraloser <Asgoth55@gmail.com>
2021-07-05 09:21:59 -0300

    feat: add QuestionRepository

---
+++ src/repositories/QuestionRepository.ts
@@ -0,0 +1,5 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Question } from "../entities/Question";
+
+@EntityRepository(Question)
+export class QuestionRepository extends Repository<Question> {}
