a1612b54ea682eca9663bfecaf23dda497765d98
Sobraloser <Asgoth55@gmail.com>
2021-07-05 21:19:37 -0300

    refact: change structure answer question

--- src/services/Answer/CreateAnswerService.ts
+++ src/services/Answer/CreateAnswerService.ts
@@ -5,7 +5,7 @@ import { QuestionRepository } from "../../repositories/QuestionRepository";
 interface CreateAnswerServiceProps {
   user: string;
   question: string;
-  answer: string;
+  answer: string[];
 }
 
 class CreateAnswerService {
@@ -13,30 +13,25 @@ class CreateAnswerService {
     const answerRepository = getCustomRepository(AnswerRepository)
     const questionRepository = getCustomRepository(QuestionRepository)
 
-    const isQuestionExists = await questionRepository.findOne(question)
+    const questions = await questionRepository.find()
 
-    if(!isQuestionExists){
-      throw new Error("This question not exist status:400")
-    }
-    const category = isQuestionExists.category
-
-    const isAlreadyAnswered = await answerRepository
-    .createQueryBuilder('answer')
-    .where('answer.user = :user', { user })
-    .andWhere('answer.question = :question', { question })
-    .getOne()
-
-    if(isAlreadyAnswered){
-      isAlreadyAnswered.answer = answer
-      await answerRepository.save(isAlreadyAnswered)
-
-      return isAlreadyAnswered
+    if(answer.length !== questions.length){
+      throw new Error(`You have to answer all questions, ${questions.length - answer.length} remaining status:400`)
     }
 
-    const newAnswer = answerRepository.create({ user, category, question, answer })
-    await answerRepository.save(newAnswer)
+    questions.forEach(async (item, index) => {
+      const id = item.id
+      const itemCategory = item.category
+      const answerToSave = answerRepository.create({ 
+        question: id, 
+        answer: answer[index], 
+        user,
+        category: itemCategory
+       })
+      await answerRepository.save(answerToSave)
+    })
 
-    return newAnswer
+    return "All questions successfully answered"
   }
 }
 export default new CreateAnswerService()
