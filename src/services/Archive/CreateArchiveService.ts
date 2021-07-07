5203f9965c50915f8af9779094ecc0483abddf7d
Sobraloser <Asgoth55@gmail.com>
2021-07-07 14:55:16 -0300

    refact: change cloudinary config in upload

--- src/services/Archive/CreateArchiveService.ts
+++ src/services/Archive/CreateArchiveService.ts
@@ -7,24 +7,28 @@ interface ArchiveProps{
   name: string;
   description?: string;
   author: string,
-  files: Express.Multer.File[]
+  files: Express.Multer.File
 }
 
 class CreateArchiveService {
   async execute({ name, author, description = null, files }: ArchiveProps){
+    if(!files) throw new Error('No File detected status:400')
+
+    if(!name) throw new Error('No name detected status:400')
+
+    if(!author) throw new Error('No author detected status:400')
+    
     const repository = getCustomRepository(ArchiveRepository)
-
-    if(files.length !== 1){
-      throw new Error("There are more than 1 archive")
-    }
-
+    
     cloudinary.uploader.upload(
-      files[0].path, 
-      { resource_type: "video", overwrite: true }
-    );
+      files.path,
+      { resource_type: "auto", overwrite: true },
+      (error, result) => {
+        if(error) throw new Error(`Error: ${error.message} status:500`)
+    });
 
     const archive = repository.create({
-      name, author, description, url: files[0].path
+      name, author, description, url: files.path
     })
     await repository.save(archive)
 
