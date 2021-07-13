505510acb962eaa1033353e6a19d7f3850456899
Sobraloser <Asgoth55@gmail.com>
2021-07-13 13:44:26 -0300

    fix: entities activity and archive

--- src/entities/Activity.ts
+++ src/entities/Activity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 import { Category } from "./Category";
 
@@ -22,6 +22,12 @@ export class Activity {
   @JoinColumn({ name: 'category' })
   @ManyToOne(() => Category, { cascade: true })
   JoinCategory: Category;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 
   constructor(){
     if(!this.id){
