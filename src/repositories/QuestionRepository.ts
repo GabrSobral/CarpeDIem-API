b177442c3636108b4514fd92a77cefe1c2ce49a0
Sobraloser <Asgoth55@gmail.com>
2021-07-23 13:21:46 -0300

    feat: add feedback repository to class

--- src/repositories/QuestionRepository.ts
+++ src/repositories/QuestionRepository.ts
@@ -1,5 +1,5 @@
-import { EntityRepository, getCustomRepository, Repository } from "typeorm";
-import { Question } from "../entities/Question";
+import { EntityRepository, Repository } from 'typeorm';
+import { Question } from '../entities/Question';
 
 @EntityRepository(Question)
 export class QuestionRepository extends Repository<Question> {}
