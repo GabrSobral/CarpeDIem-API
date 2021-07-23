541a781348270acd8260d7f5c6924cfa57cd124c
Sobraloser <Asgoth55@gmail.com>
2021-07-22 21:21:01 -0300

    feat: add feedback to randomCategory

--- src/services/ActivitiesOfTheDay/ListActivitiesForMeService/handleRandomCategory.ts
+++ src/services/ActivitiesOfTheDay/ListActivitiesForMeService/handleRandomCategory.ts
@@ -1,29 +1,36 @@
-import { Answer } from "../../../entities/Answer"
+import { Answer } from '../../../entities/Answer';
 
 class handleRandomCategory {
-  execute(answersSum: number[], userAnswers: Answer[]){
-    const random = Math.round(Math.random() * answersSum[answersSum.length - 1])
-    let currentCategory = ""
+  execute(answersSum: number[], userAnswers: Answer[]) {
+    const random = Math.round(
+      Math.random() * answersSum[answersSum.length - 1]
+    );
+    let currentCategory = '';
 
-    for(let i = 0; i < answersSum.length; i++){
-      if(i === 0){
-        if(random <= answersSum[i]){ 
-          currentCategory = userAnswers[i].category 
-          break
+    for (let i = 0; i < answersSum.length; i++) {
+      if (i === 0) {
+        if (random <= answersSum[i]) {
+          currentCategory = userAnswers[i].category;
+          break;
         }
       }
 
-      if(random > answersSum[i] && random <= answersSum[i + 1]){
-        currentCategory = userAnswers[i + 1].category
-        break
+      if (random > answersSum[i] && random <= answersSum[i + 1]) {
+        currentCategory = userAnswers[i + 1].category;
+        break;
       }
 
-      if(random <= answersSum[i]){
-        currentCategory = userAnswers[i].category
-        break
+      if (random <= answersSum[i]) {
+        currentCategory = userAnswers[i].category;
+        break;
+      }
+      if (random <= answersSum[answersSum.length - 1]) {
+        console.log('feedback');
+        currentCategory = 'feedback';
+        break;
       }
     }
-    return currentCategory
+    return currentCategory;
   }
 }
-export default new handleRandomCategory().execute
+export default new handleRandomCategory().execute;
