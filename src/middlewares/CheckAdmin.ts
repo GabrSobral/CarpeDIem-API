0d4613b6ec7224eb4399b445a84375230b73aed6
Sobraloser <Asgoth55@gmail.com>
2021-07-27 13:28:34 -0300

    feat: add checkAdmin middleware

---
+++ src/middlewares/CheckAdmin.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import handleGetRepositories from '../utils/handleGetRepositories';
+
+export async function CheckAdmin(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const { userRepository } = handleGetRepositories()
+  const user_id = request.user_id
+
+  const user = await userRepository.findOne(user_id)
+
+  if(user.role !== 'admin'){
+    return response.status(401).json({ error: "unauthorized" })
+  }
+  
+  return next()
+}
