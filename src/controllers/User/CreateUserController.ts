eae6e70b635c61e82f54c6cafd6a8d1cd8acc391
Sobraloser <Asgoth55@gmail.com>
2021-07-27 13:27:02 -0300

    feat: add role to user controller

--- src/controllers/User/CreateUserController.ts
+++ src/controllers/User/CreateUserController.ts
@@ -3,9 +3,9 @@ import CreateUserService from "../../services/User/CreateUserService";
 
 class CreateUserController {
   async handle(request: Request, response: Response){
-    const { name, email, password } = request.body;
+    const { name, email, password, role } = request.body;
 
-    const user = await CreateUserService.execute({ name, email, password })
+    const user = await CreateUserService.execute({ name, email, password, role })
 
     return response.json(user)
   }
