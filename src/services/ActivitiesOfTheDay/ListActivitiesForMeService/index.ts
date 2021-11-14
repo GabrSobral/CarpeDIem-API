8fc02fed6f78ad02cc5354736a0ab764725fd9e4
Sobraloser <Asgoth55@gmail.com>
2021-11-13 21:08:25 -0300

    refact: listActivitiesForMeService

--- src/services/ActivitiesOfTheDay/ListActivitiesForMeService/index.ts
+++ src/services/ActivitiesOfTheDay/ListActivitiesForMeService/index.ts
@@ -1,5 +1,3 @@
-import { Activity } from "../../../entities/Activity";
-
 import handleGetRepositories from "../../../utils/handleGetRepositories";
 import RandomInteger from "../../../utils/RandomInteger";
 import handlePutFilesInActivities from "../../../utils/handlePutFilesInActivities";
@@ -35,57 +33,45 @@ class ListActivitiesForMeTest {
     const answersSum = await handleAnswersSum(user_id, hasFeedback);
 
     for (let i = 0; i < userData.quantity_of_activities; i++) {
-      try {
-        const category = handleRandomCategory({answersSum});
+      const category = handleRandomCategory({answersSum});
       
-        if(category === "FEEDBACK") {
-          const feedbackFiltered = [];
-          
-          goodFeedbacks.forEach((feedback) => {
-            const dontExists = orderedActivities.some((item) => item?.id !== feedback.activity);
-            dontExists && feedbackFiltered.push(feedback);
-          });
-  
-          if (feedbackFiltered.length === 0){ i--;  continue; }
-  
-          const randomActivityOfCategory = RandomInteger(0, feedbackFiltered.length - 1);
-  
-          const activityObject = {
-            ...feedbackFiltered[randomActivityOfCategory].JoinActivity,
-            JoinCategory: feedbackFiltered[randomActivityOfCategory].JoinCategory,
-          }
-          orderedActivities.push(activityObject);
-          continue;
-        }
-
-        const activitiesOfCategory = await activitiesRepository
-        .find({ where: { category }, relations: ["JoinCategory"] });
-
-        const ActivitiesFiltered: Activity[] = [];
-
-        activitiesOfCategory.forEach((activity) => {
-          const dontExists = orderedActivities.every((item) => item?.id !== activity?.id);
-          let leavePass = true;
-
-          if((RandomInteger(0, 10) < 7) && (badFeedbacks.length !== 0)){
-            const containBadFeedbacks = 
-              badFeedbacks.every((item) => item.activity === activity?.id);
-
-            containBadFeedbacks && (leavePass = false);
-          };
-          
-          ( dontExists && leavePass ) && ActivitiesFiltered.push(activity);
+      if( category === "FEEDBACK" ) {
+        const feedbackFiltered = [];
+        
+        goodFeedbacks.forEach((feedback) => {
+          const containGoodFeedbacks = orderedActivities.some((item) => item?.id === feedback.activity);
+          !containGoodFeedbacks && feedbackFiltered.push(feedback);
         });
 
-        const randomActivityOfCategory = RandomInteger(0, ActivitiesFiltered.length - 1);
+        if (feedbackFiltered.length === 0){ i--;  continue; }
 
-        orderedActivities.push(ActivitiesFiltered[randomActivityOfCategory]);
-      } catch{
-        i--;
+        const randomActivityOfCategory = RandomInteger(0, feedbackFiltered.length - 1);
+
+        const activityObject = {
+          ...feedbackFiltered[randomActivityOfCategory].JoinActivity,
+          JoinCategory: feedbackFiltered[randomActivityOfCategory].JoinCategory,
+        }
+        orderedActivities.push(activityObject);
         continue;
       }
+
+      const activitiesOfCategory = await activitiesRepository
+        .find({ where: { category }, relations: ["JoinCategory"] });
+
+      if(badFeedbacks.length !== 0) {
+        activitiesOfCategory.forEach((activity, index) => {
+          if(orderedActivities.some((item) => item?.id === activity?.id)) { return }
+          
+          if(RandomInteger(0, 10) < 7)
+            badFeedbacks.some((item) => item.activity === activity?.id) &&
+              activitiesOfCategory.splice(index, 1)
+        });
+      }
+
+      const randomActivityOfCategory = RandomInteger(0, activitiesOfCategory.length - 1);
+
+      orderedActivities.push(activitiesOfCategory[randomActivityOfCategory]);
     }
-    
     await handleSaveInDB.users(userData)
     await handleSaveInDB.activities(orderedActivities, user_id)
 
